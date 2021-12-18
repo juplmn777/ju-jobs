130702bd311b9ed14bda22a038457134e7dfff94
juplmn777 <julien.plumyoen@gmail.com>
2021-12-18 09:36:10 +0100

    fetch datas & conditionaly show them

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,14 +1,19 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link
-      :to="{
-        name: 'JobDetails',
-        params: { id: job.id },
-      }"
-    >
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link
+        :to="{
+          name: 'JobDetails',
+          params: { id: job.id },
+        }"
+      >
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading jobs...</p>
   </div>
 </template>
 
@@ -16,24 +21,14 @@
 export default {
   data() {
     return {
-      jobs: [
-        {
-          title: 'Front-End Developer',
-          id: 1,
-          details: 'lorem',
-        },
-        {
-          title: 'Web Designer',
-          id: 2,
-          details: 'lorem',
-        },
-        {
-          title: 'Back-End Developer',
-          id: 3,
-          details: 'lorem',
-        },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs')
+      .then((response) => response.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
