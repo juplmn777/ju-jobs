94cc25dc225d66e9b278e541984204c836fbc80e
juplmn777 <julien.plumyoen@gmail.com>
2021-12-18 09:49:18 +0100

    style added in JobDetails.vue

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,11 +1,10 @@
 <template>
-  <div v-if="job">
+  <div v-if="job" class="job-card">
     <h1>{{ job.title }}</h1>
-    <p>The job ID is {{ id }}</p>
     <p>{{ job.details }}</p>
   </div>
   <div v-else>
-    <p>Loading job details...</p>
+    <p class="load-job-card">Loading job details...</p>
   </div>
 </template>
 
@@ -26,4 +25,22 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.job-card {
+  max-width: 500px;
+  margin: 50px auto;
+  padding: 20px;
+  background: #2c3e50;
+  border-radius: 10px;
+  color: #f1f1f1;
+}
+
+h1 {
+  font-size: 3rem;
+}
+
+p {
+  font-size: 1.5rem;
+  text-align: left;
+}
+</style>
